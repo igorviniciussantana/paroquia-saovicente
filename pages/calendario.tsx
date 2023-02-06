ea5461a3b59f53c36235c229a1b69f1dd6d90f6d
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-02-05 21:09:18 -0400

    feat: change calendar route

--- pages/calendario.tsx
+++ pages/calendario.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import Table from "../components/Table/Table";
-import {MonumentBlack, MonumentLight, newYork} from './../src/imports'
+import {MonumentBlack, MonumentLight, newYork} from '../src/imports'
 import Footer from "../components/Footer/Footer";
 export default function Home() {
   return (
