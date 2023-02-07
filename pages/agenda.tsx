5e5dc0d5c4bf93aeedfab6c8340bb110aa905dbb
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-02-06 21:41:15 -0400

    feat: add event delete

--- pages/agenda.tsx
+++ pages/agenda.tsx
@@ -2,35 +2,41 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import Table from "../components/Table/Table";
-import {MonumentBlack, MonumentLight, newYork} from '../src/imports'
+import { MonumentBlack, MonumentLight, newYork } from "../src/imports";
 import Footer from "../components/Footer/Footer";
 import { useState } from "react";
 import CreateEventModal from "../components/Event/createEventModal/createEvent";
 import { GetServerSideProps } from "next";
 import { api } from "./api/api";
 
-interface EventsAtribute{
-    name: string;
-    Data: string;
-    Time: string;
+interface EventsAtribute {
+  name: string;
+  Data: string;
+  Time: string;
 }
 
-interface Event{
-    attributes: EventsAtribute
+interface Event {
+  id: number;
+  attributes: EventsAtribute;
 }
 
-interface AgendaProps{
-    events: Event[]
+interface AgendaProps {
+  events: Event[];
 }
 
-export default function Agenda({events} : AgendaProps) {
+export default function Agenda({ events }: AgendaProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  function showEventModal() {
+    setIsModalOpen(!isModalOpen);
+  }
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    function showEventModal(){
-        setIsModalOpen(!isModalOpen)
-    }
+  const deleteEvent = async function (id: number) {
+    const response = await api
+      .delete(`events/${id}`)
+      .then((response) => alert("Evento excluido"))
+      .catch((err) => alert("Houve um erro"));
+  };
   return (
     <>
       <Head>
@@ -45,42 +51,43 @@ export default function Agenda({events} : AgendaProps) {
 
       <div className={styles.bgImage}></div>
       <main className={styles.main}>
-        <h1 className={`${newYork.className} ${styles.title}`}>
-          Agenda
-        </h1>
+        <h1 className={`${newYork.className} ${styles.title}`}>Agenda</h1>
         <h3 className={`${MonumentLight.className} ${styles.subtitle}`}>
           PARÓQUIA SÃO VICENTE DE PAULO - NOVA ANDRADINA/MS
         </h3>
 
         <img src="/images/sao-vicente.jpg" className={styles.saoVicenteImage} />
 
-       <button onClick={showEventModal}>
-        Adicionar Evento
-       </button>
+        <button onClick={showEventModal}>Adicionar Evento</button>
 
-       <CreateEventModal display={isModalOpen ? 'flex' : 'none'} modalChanger={showEventModal}/>
+        <CreateEventModal
+          display={isModalOpen ? "flex" : "none"}
+          modalChanger={showEventModal}
+        />
 
-       {events.map(event => {
-        return(
-            <h1>{event.attributes.name}</h1>
-        )
-       })}
+        {events.map((event) => {
+          return (
+            <>
+              <h1>{event.attributes.name}</h1>
+              <h1>{new Date(event.attributes.Data).toLocaleString("pt-BR")}</h1>
+              <button onClick={(e) => deleteEvent(event.id)}>Deletar</button>
+            </>
+          );
+        })}
         <Footer />
       </main>
-
-      
     </>
   );
 }
 
-export const getServerSideProps : GetServerSideProps = async() => {
-    const response = await api.get('/events')
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await api.get("/events");
 
-    const eventsData = await response.data
+  const eventsData = await response.data;
 
-    return{
-        props: {
-            events: eventsData.data,
-        }
-    }
-}
+  return {
+    props: {
+      events: eventsData.data,
+    },
+  };
+};
--- pages/api/api.ts
+++ pages/api/api.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export const api = axios.create({
 
-baseURL: 'http://localhost:1337/api/'
+baseURL: 'http://localhost:1337/api'
 
 })
